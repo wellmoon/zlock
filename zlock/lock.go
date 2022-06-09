a53e9c2ae9e997361d65645005b80a20c4756e3c
wellmoon <33535324+wellmoon@users.noreply.github.com>
2022-06-09 15:13:46 +0800

    init

---
+++ zlock/lock.go
@@ -0,0 +1,31 @@
+package zlock
+
+import "sync"
+
+type ZLockMap struct {
+	SyncMap sync.Map
+}
+
+func New(m sync.Map) *ZLockMap {
+	return &ZLockMap{m}
+}
+
+func (zm *ZLockMap) Lock(key interface{}) {
+	val, ok := zm.SyncMap.Load(key)
+	if ok {
+		l := val.(*sync.Mutex)
+		l.Lock()
+	} else {
+		l := &sync.Mutex{}
+		l.Lock()
+		zm.SyncMap.Store(key, l)
+	}
+}
+
+func (zm *ZLockMap) UnLock(key interface{}) {
+	val, ok := zm.SyncMap.Load(key)
+	if ok {
+		l := val.(*sync.Mutex)
+		l.Unlock()
+	}
+}
